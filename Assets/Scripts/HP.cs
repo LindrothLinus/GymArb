6db4144a6a6343cd86d6b1da603443701cf5085c
LindrothLinus <112934866+LindrothLinus@users.noreply.github.com>
2024-01-29 13:35:32 +0100

    inviframe

--- Assets/Scripts/HP.cs
+++ Assets/Scripts/HP.cs
@@ -2,21 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class HP : MonoBehaviour
 {
     public int Hp;
     public Rigidbody2D RB;
     public GameOver GO;
     [SerializeField]LiveManeger LV;
+    private int frames =30;
+    private bool tookDamage=false;
     // Start is called before the first frame update
     void Start()
     {
         LV.LifeCount = Hp;
     }
 
-    // Update is called once per frame
-    void Update()
+	private void FixedUpdate()
+	{
+        if (tookDamage)
+        {
+            frames++;
+        }
+        if (frames > 100)
+        {
+            tookDamage = false;
+            frames = 0;
+        }
+    }
+
+	// Update is called once per frame
+	void Update()
     {
+
         if (Hp <= 0)
         {
             GetComponent<Movmen>().enabled = false;
@@ -36,8 +53,17 @@ public class HP : MonoBehaviour
         
         if (other.gameObject.CompareTag("Enemy") && RB.velocity.y>=0)
         {
-            Hp--;
-            LV.LifeCount--;
+            //if (!(frames > 30)) return;
+            if(tookDamage == false)
+			{
+                Hp--;
+                LV.LifeCount--;
+                tookDamage = true;
+                frames = 0;
+            }
+
+
+            
         }
 
 
